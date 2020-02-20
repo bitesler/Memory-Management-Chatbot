bdbb06a7b2d283e062a38db77b26313423c4dee4
Commo Dore <cmmdore64@gmail.com>
2020-02-20 03:03:56 -0800

    make _chatBot a unique pointer

--- src/graphnode.h
+++ src/graphnode.h
@@ -17,10 +17,13 @@ private:
 
     // data handles (owned)
     std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    std::vector<std::unique_ptr <GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
+    //std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
+    //ChatBot *_chatBot;
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
-    ChatBot *_chatBot;
+    std::unique_ptr<ChatBot> _chatBot;
 
     ////
     //// EOF STUDENT CODE
@@ -49,7 +52,8 @@ public:
     //// STUDENT CODE
     ////
 
-    void MoveChatbotHere(ChatBot *chatbot);
+    //void MoveChatbotHere(ChatBot *chatbot);
+    void MoveChatbotHere(ChatBot chatbot);
 
     ////
     //// EOF STUDENT CODE
