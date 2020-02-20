3017106f32f676b15f6ed4e3ae785ca864ab1afb
Commo Dore <cmmdore64@gmail.com>
2020-02-20 12:32:18 -0800

    Memory Management Chatbot written in C++

--- src/graphnode.h
+++ src/graphnode.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <memory>
 #include "chatbot.h"
 
 
@@ -16,13 +17,12 @@ private:
     ////
 
     // data handles (owned)
-    std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    //std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
     std::vector<std::unique_ptr <GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
-    //std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
-    //ChatBot *_chatBot;
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
+    //ChatBot *_chatBot;
     std::unique_ptr<ChatBot> _chatBot;
 
     ////
@@ -47,8 +47,9 @@ public:
     // proprietary functions
     void AddToken(std::string token); // add answers to list
     void AddEdgeToParentNode(GraphEdge *edge);
-    void AddEdgeToChildNode(GraphEdge *edge);
-
+    //void AddEdgeToChildNode(GraphEdge *edge);
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
+    
     //// STUDENT CODE
     ////
 
